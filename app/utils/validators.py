436b3f148a202888cc6d20ab70c55d80f8f02476
Jatin Joshi <jatinjoshi105@gmail.com>
2026-05-13 12:49:14 +0530

    Restructed Complete

--- app/utils/validators.py
+++
@@ -1,9 +0,0 @@
-def required_fields(data, keys):
-    return [key for key in keys if not data.get(key)]
-
-
-def is_positive_int(value):
-    try:
-        return int(value) > 0
-    except (TypeError, ValueError):
-        return False
